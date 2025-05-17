c75c8a4497554955792df8355a30829fcf67ab2b
Kritikaa <tp070704@mail.apu.edu.my>
2025-05-17 10:25:52 +0800

    deleted admin

--- src/main/Admin/core/AdminMain.java
+++
@@ -1,7 +0,0 @@
-package Admin;
-
-public class AdminMain {
-    public static void main(String[] args) {
-        new LoginUI().setVisible(true);
-    }
-}
--- src/main/utils/Constants.java
+++
@@ -1,6 +0,0 @@
-package utils;
-
-public class Constants {
-    public static final String USER_FILE = "users.txt";
-    public static final String LOG_FILE = "logs.txt";
-}
